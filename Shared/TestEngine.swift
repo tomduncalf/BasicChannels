9b38f48d55bc0510b78a5649fd989e3e8928e55c
Tom Duncalf <tom@tomduncalf.com>
2021-05-28 15:54:03 +0100

    Basssss

--- Shared/TestEngine.swift
+++ Shared/TestEngine.swift
@@ -14,6 +14,7 @@ class TestEngine {
     let engine = AudioEngine()
     let drumSampler = AppleSampler()
     let chordSampler: Sampler
+    let bassSampler: Sampler
     let sequencer = Sequencer()
     var callbackInst = CallbackInstrument()
     let mixer = Mixer()
@@ -23,8 +24,6 @@ class TestEngine {
     var lastChordHadAttack = false
 
     init() {
-        let chordSample: AVAudioFile
-        
         do {
             let sampleNames = ["bass_drum_C1", "snare_D1", "closed_hi_hat_F#1"]
             let sampleFiles = try sampleNames.map { (name) -> AVAudioFile in
@@ -36,22 +35,6 @@ class TestEngine {
             Log("Error loading sample \(err)")
         }
 
-//        chordSample = try! AVAudioFile(forReading: (Bundle.main.resourceURL?.appendingPathComponent("Samples/synth_C3.wav"))!)
-//
-//        let desc = SampleDescriptor(noteNumber: 24,
-//                                 noteFrequency: Float(TuningTable().frequency(forNoteNumber: 24)),
-//                                 minimumNoteNumber: 0,
-//                                 maximumNoteNumber: 127,
-//                                 minimumVelocity: 0,
-//                                 maximumVelocity: 127,
-//                                 isLooping: false,
-//                                 loopStartPoint: 0.0,
-//                                 loopEndPoint: 1.0,
-//                                 startPoint: 0.0,
-//                                 endPoint: 0.0)
-        
-        chordSample = try! AVAudioFile(forReading: (Bundle.main.resourceURL?.appendingPathComponent("Samples/AKWF/AKWF_0010/AKWF_0908.wav"))!)
-                                       
         let desc = SampleDescriptor(noteNumber: 26,
                                  noteFrequency: 44100.0/600,
                                  minimumNoteNumber: 0,
@@ -63,9 +46,14 @@ class TestEngine {
                                  loopEndPoint: 1.0,
                                  startPoint: 0.0,
                                  endPoint: 0.0)
-                                       
+
+        let chordSample = try! AVAudioFile(forReading: (Bundle.main.resourceURL?.appendingPathComponent("Samples/AKWF/AKWF_0010/AKWF_0908.wav"))!)
         chordSampler = Sampler(sampleDescriptor: desc, file: chordSample)
         chordSampler.buildSimpleKeyMap()
+        
+        let bassSample = try! AVAudioFile(forReading: (Bundle.main.resourceURL?.appendingPathComponent("Samples/AKWF/AKWF_bw_sin/AKWF_sin_0001.wav"))!)
+        bassSampler = Sampler(sampleDescriptor: desc, file: bassSample)
+        bassSampler.buildSimpleKeyMap()
         
         let drumTrack = sequencer.addTrack(for: drumSampler)
         drumTrack.length = 4
@@ -86,25 +74,43 @@ class TestEngine {
         
         chordSampler.filterEnable = 1
         chordSampler.filterCutoff = 0
+        chordSampler.filterResonance = 0.3
         chordSampler.filterStrength = 1000
         chordSampler.filterAttackDuration = 0
         
         chordSampler.masterVolume = 0.2
         
         let chordTrack = sequencer.addTrack(for: chordSampler)
-        drumTrack.length = 4
+        chordTrack.length = 4
         let baseNote: UInt8 = 48
         chordTrack.sequence.add(noteNumber: baseNote + 0, position: 0.5, duration: 1)
         chordTrack.sequence.add(noteNumber: baseNote + 3, position: 0.5, duration: 1)
         chordTrack.sequence.add(noteNumber: baseNote + 7, position: 0.5, duration: 1)
-
-//        chordFilter = MoogLadder(chordSampler, cutoffFrequency: 5000, resonance: 0.5)
+        
+        bassSampler.attackDuration = 0
+        bassSampler.decayDuration = 0.5
+        bassSampler.sustainLevel = 1.0
+        bassSampler.releaseDuration = 0.3
+        
+        bassSampler.filterEnable = 1
+        bassSampler.filterCutoff = 100
+        bassSampler.filterStrength = 0
+        bassSampler.filterAttackDuration = 0
+        
+        bassSampler.masterVolume = 0.2
+        
+        let bassTrack = sequencer.addTrack(for: bassSampler)
+        bassTrack.length = 4
+        let baseBassNote: UInt8 = 24
+        bassTrack.sequence.add(noteNumber: baseBassNote + 0, position: 0, duration: 2)
+//        bassTrack.sequence.add(noteNumber: baseBassNote + 0, position: 2.5, duration: 0.5)
+        bassTrack.sequence.add(noteNumber: baseBassNote + 7, position: 3, duration: 0.9)
         
         chordDelay = Delay(chordSampler)
-        chordDelay.feedback = 70
-        chordDelay.time = 0.395
+        chordDelay.feedback = 80
+        chordDelay.time = (60 / 120) * (3 / 4)
         chordDelay.dryWetMix = 40
-        chordDelay.lowPassCutoff = 1500
+        chordDelay.lowPassCutoff = 1200
                 
         callbackInst = CallbackInstrument(midiCallback: { (status, note, _) in
             if (status != 128) {
@@ -127,6 +133,7 @@ class TestEngine {
 
         mixer.addInput(drumSampler)
         mixer.addInput(chordDelay)
+        mixer.addInput(bassSampler)
         mixer.addInput(callbackInst)
         
         engine.output = mixer
